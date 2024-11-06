fd52965fe44e1dad87ebdded7c55c2fb8466e34c
Gabriela Machado Ribeiro <gabrielaribeiro-ieg@instituto.ad>
2024-11-06 03:54:13 -0300

    Ajustes nas funções

--- RPA_SQL.py
+++ RPA_SQL.py
@@ -55,7 +55,7 @@ def colunas_tabela_destino(table_name, conn_params):
         if conn:
             cur.close()
             conn.close()
- 
+
 # Função para criptografar a senha usando a API fornecida
 def get_encrypted_password(password):
     url = "https://api-spring-boot-trocatine.onrender.com/users/encrypt-password"
@@ -69,8 +69,8 @@ def get_encrypted_password(password):
             return data["data"]["password"]
     print("Erro ao criptografar senha:", response.status_code)
     return None
-
 # Função para inserir informações na tabela de destino 'Users'
+
 def inserir_info_tabela_destino_usuario(df_usuarios, df_adm, conn_params):
     conn = None
     try:
@@ -80,11 +80,20 @@ def inserir_info_tabela_destino_usuario(df_usuarios, df_adm, conn_params):
         # Obter usuários administradores para definir o campo 'admin'
         ids_adm = set(df_adm["idusuario"].values)
         
-        # Manter rastreamento dos apelidos para evitar duplicidade
+        # Manter rastreamento dos apelidos e emails para evitar duplicidade
         apelidos_existentes = set()
         
+        # Obter emails já existentes na tabela users
+        cur.execute("SELECT email FROM users")
+        emails_existentes = set(row[0] for row in cur.fetchall())
+
         for index, row in df_usuarios.iterrows():
-            # Nome
+            # Verifica se o email já existe
+            if row['email'] in emails_existentes:
+                print(f"O email '{row['email']}' já existe. Pular inserção.")
+                continue
+            
+            # Nome e criação de nickname único
             first_name = row['nome']
             last_name = row['sobrenome']
             nickname_base = f"{first_name}_{last_name}"
@@ -95,7 +104,7 @@ def inserir_info_tabela_destino_usuario(df_usuarios, df_adm, conn_params):
                 num += 1
             apelidos_existentes.add(nickname)
             
-            # Admin
+            # Verificar se é administrador
             admin = row['id'] in ids_adm
             
             # Criptografar senha
@@ -105,6 +114,9 @@ def inserir_info_tabela_destino_usuario(df_usuarios, df_adm, conn_params):
             query_users = (f"INSERT INTO users (first_name, last_name, email, cpf, birth_date, admin, nickname, password)"
                            f" VALUES (%s, %s, %s, %s, %s, %s, %s, %s)")
             cur.execute(query_users, (first_name, last_name, row['email'], row['cpf'], row['dt_nascimento'], admin, nickname, encrypted_password))
+            
+            # Adicionar o email ao conjunto para evitar duplicatas na execução
+            emails_existentes.add(row['email'])
         
         conn.commit()
         print("Dados inseridos com sucesso na tabela Users")
@@ -160,21 +172,38 @@ def transferindo_info_usuario(parametros_extracao, parametros_destino):
     else:
         print("Erro ao extrair dados das tabelas de origem")
 
-def inserir_info_tabela_destino_category(df, table_name, conn_params, target_columns):
+def inserir_info_tabela_destino_category(df, table_name, conn_params, target_columns): 
     conn = None
     try:
         conn = psycopg2.connect(**conn_params)
         cur = conn.cursor()
         
-        placeholders  = ', '.join(['%s'] * len(target_columns))
-
+        # Consulta para verificar o maior id_category já existente
+        cur.execute(f"SELECT MAX(id_category) FROM {table_name}")
+        max_id = cur.fetchone()[0] or 0
+        
+        # Query para inserir dados
+        placeholders = ', '.join(['%s'] * len(target_columns))
         colunas_str = ', '.join(target_columns)
+        query_insert = f"INSERT INTO {table_name} ({colunas_str}) VALUES ({placeholders})"
+        
+        # Query para verificar se já existe um registro com o mesmo name
+        query_check = f"SELECT EXISTS (SELECT 1 FROM {table_name} WHERE name = %s)"
 
-        query = (f"INSERT INTO {table_name} ({colunas_str}) VALUES ({placeholders})")
- 
-        for _, row in df.iterrows(): 
-            cur.execute(query, tuple(row))
- 
+        for _, row in df.iterrows():
+            tipo_produto = row['tipo_produto']  # Obtém o tipo_produto do DataFrame
+            
+            # Verifica se o name já existe
+            cur.execute(query_check, (tipo_produto,))
+            exists = cur.fetchone()[0]
+            
+            if not exists:
+                max_id += 1  # Incrementa o id_category
+                cur.execute(query_insert, (max_id, tipo_produto))  # Insere id_category e name
+                print(f"Registro inserido: {max_id} - {tipo_produto}")
+            else:
+                print(f"O nome '{tipo_produto}' já existe. Pular inserção.")
+        
         conn.commit()
         print(f"Dados inseridos com sucesso na tabela {table_name}")
     except Exception as e:
@@ -206,27 +235,31 @@ def inserir_info_tabela_destino_tag(df, table_name, conn_params, target_columns)
         conn = psycopg2.connect(**conn_params)
         cur = conn.cursor()
         
-        print(target_columns)
-        target_columns= target_columns[1:]
+        target_columns = target_columns[1:]  # Ignora o primeiro elemento de target_columns
         colunas_str = ', '.join(target_columns)
         
+        # Consulta para obter os valores existentes na coluna "name"
         cur.execute(f"SELECT DISTINCT name FROM {table_name}")
         valores_existentes = set(row[0] for row in cur.fetchall())  
 
         for i in df:
-            if i != 'id' and i != 'idtipo_produto':
+            if i != 'id' and i != 'idcategoria':
                 
+                # Obtém valores únicos da coluna atual, ignorando nulos
                 valores_unicos = set(df[i].dropna())
 
                 for j in valores_unicos:
                     if j not in valores_existentes:  
-                        print(j)  
-                        query = (f"INSERT INTO {table_name} ({colunas_str}) VALUES ('{i.capitalize()}','{j}')")
+                        # Monta e executa a query de inserção
+                        query = f"INSERT INTO {table_name} ({colunas_str}) VALUES ('{i.capitalize()}', '{j}')"
                         cur.execute(query)  
-
-            else:
-                pass
-      
+                        print(f"Registro inserido: {j}")
+                        
+                        # Adiciona o novo valor à lista de valores existentes para evitar duplicatas
+                        valores_existentes.add(j)
+                    else:
+                        print(f"O nome '{j}' já existe. Pular inserção.")
+        
         conn.commit()
         print(f"Dados inseridos com sucesso na tabela {table_name}")
 
@@ -235,7 +268,8 @@ def inserir_info_tabela_destino_tag(df, table_name, conn_params, target_columns)
     finally:
         if conn:
             cur.close()
-            conn.close() 
+            conn.close()
+
  
 def transferindo_info_tag(tabelas_mapeadas, parametros_extracao, parametros_destino):
 
@@ -277,4 +311,6 @@ tabela_category = {
     "categoria":"categories"
 }
 
+transferindo_info_tag(tabela_tag, parametros_extracao, parametros_destino)
+transferindo_info_category(tabela_category,parametros_extracao,parametros_destino)
 transferindo_info_usuario(parametros_extracao, parametros_destino)
